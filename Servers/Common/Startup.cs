4b2a8a2d8a342018c09a3039a4f32defdf5a6409
Gustavo Armenta <gustavo.armenta@gmail.com>
2013-10-23 10:38:12 -0700

    first commit

---
+++ Servers/Common/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR.Samples.Raw;
+using Microsoft.AspNet.SignalR.Server;
+using Microsoft.Owin;
+using Owin;
+
+namespace Microsoft.AspNet.SignalR.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapConnection<RawConnection>("/raw", new ConnectionConfiguration { EnableCrossDomain = true });
+            app.MapHubs(new HubConfiguration { EnableCrossDomain = true });
+        }
+    }
+}
---
+++ Servers/Microsoft.AspNet.SignalR.Server.Web/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR.Samples.Raw;
+
+namespace Microsoft.AspNet.SignalR.Server.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.MapConnection<RawConnection>("/raw", "/raw", new ConnectionConfiguration { EnableCrossDomain = true });
+            RouteTable.Routes.MapHubs(new HubConfiguration { EnableCrossDomain = true });
+        }
+    }
+}
